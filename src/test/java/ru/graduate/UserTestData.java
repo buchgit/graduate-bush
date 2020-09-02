0361871ea51ba341e11780fda2f70a8508cff2de
buchgit <buchnorilsk@gmail.com>
2020-09-02 16:20:39 +0300

    AdminProfileControllerTest

--- src/test/java/ru/graduate/UserTestData.java
+++ src/test/java/ru/graduate/UserTestData.java
@@ -1,9 +1,16 @@
 package ru.graduate;
 
+import org.springframework.test.web.servlet.ResultMatcher;
 import ru.graduate.model.Role;
 import ru.graduate.model.User;
+import ru.graduate.web.json.JsonUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+import static ru.graduate.TestUtil.readFromJsonMvcResult;
+import static org.assertj.core.api.Assertions.assertThat;
 import static ru.graduate.model.AbstractBaseEntity.START_SEQ;
+import static ru.graduate.TestUtil.readListFromJsonMvcResult;
 
 public class UserTestData {
 
@@ -13,7 +20,41 @@ public class UserTestData {
 
     public static final User USER = new User(USER_ID, "User", "user@gmail.com", "user", Role.USER);
     public static final User ADMIN = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ADMIN, Role.USER);
+    public static final User UPDATED = new User(ADMIN_ID, "Admin", "admin@gmail.com", "newPass", Role.ADMIN, Role.USER);
+    public static User NEW_USER = new User( null,"NEW", "new@gmail.com", "newPass",  Role.USER);
+
+    public static final List<User> USERS = List.of(USER,ADMIN);
 
     public static TestMatcher<User> USER_MATCHER = TestMatcher.usingFieldsWithIgnoringAssertions(User.class, "enabled","registered", "votes", "password","roles");
 
+//    public static void assertMatch(Iterable<User> actual, User... expected) {
+//        assertMatch(actual, List.of(expected));
+//    }
+    public static void assertMatch(User actual, User expected) {
+        assertThat(actual).isEqualToIgnoringGivenFields(expected, "registered", "votes", "password");
+    }
+
+//    public static void assertMatch(Iterable<User> actual, User... expected) {
+//        assertMatch(actual, List.of(expected));
+//    }
+
+    public static void assertMatch(Iterable<User> actual, Iterable<User> expected) {
+        assertThat(actual).usingElementComparatorIgnoringFields("registered", "votes", "password").isEqualTo(expected);
+    }
+
+//    public static void assertMatch(Iterable<User> actual, User... expected) {
+//        assertMatch(actual, List.of(expected));
+//    }
+
+    public static ResultMatcher contentJson(User... expected) {
+        return result -> assertMatch(readListFromJsonMvcResult(result, User.class), List.of(expected));
+    }
+
+    public static ResultMatcher contentJson(User expected) {
+        return result -> assertMatch(readFromJsonMvcResult(result, User.class), expected);
+    }
+
+    public static String jsonWithPassword(User user, String passw) {
+        return JsonUtil.writeAdditionProps(user, "password", passw);
+    }
 }
--- src/test/java/ru/graduate/web/AdminProfileControllerTest.java
+++ src/test/java/ru/graduate/web/AdminProfileControllerTest.java
@@ -1,44 +1,135 @@
 package ru.graduate.web;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.graduate.UserTestData;
+import ru.graduate.model.Role;
+import ru.graduate.model.User;
+import ru.graduate.service.UserService;
+import ru.graduate.web.json.JsonUtil;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static ru.graduate.UserTestData.*;
+import static ru.graduate.TestUtil.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-class AdminProfileControllerTest  extends AbstractControllerTest{
+class AdminProfileControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = AdminProfileController.REST_URL;
+
+    @Autowired
+    private UserService userService;
 
     @Test
-    void create() {
+    void create() throws Exception {
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN))
+                .content(UserTestData.jsonWithPassword(NEW_USER, "newPass")))
+                .andExpect(status().isCreated());
+        User created = userService.getByEmail(NEW_USER.getEmail());
+        NEW_USER.setId(created.getId());
+        USER_MATCHER.assertMatch(created, NEW_USER);
     }
 
     @Test
-    void update() {
+    void update() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN))
+                .content(UserTestData.jsonWithPassword(UPDATED, "newPass")))
+                .andExpect(status().isOk());
+
+        USER_MATCHER.assertMatch(userService.getById(ADMIN_ID), UPDATED);
     }
 
     @Test
-    void delete() {
+    void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL + "/" + USER_ID)
+                .with(userHttpBasic(ADMIN)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
     }
 
     @Test
-    void get() {
+    void deleteNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL + 1)
+                .with(userHttpBasic(ADMIN)))
+                .andExpect(status().isNotFound())
+                .andDo(print());
     }
 
     @Test
-    void getAll() {
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "/" + USER_ID)
+                .with(userHttpBasic(ADMIN)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(contentJson(USER));
     }
 
     @Test
-    void getByEmail() {
+    void getAll() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL)
+                .with(userHttpBasic(ADMIN)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(contentJson(USER, ADMIN));
     }
 
     @Test
-    void enable() {
+    void getByEmail() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "/email")
+                .param("email", ADMIN.getEmail())
+                .with(userHttpBasic(ADMIN)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(contentJson(ADMIN));
     }
 
-//    @Test
-//    void getNotFound() throws Exception {
-//        perform(MockMvcRequestBuilders.get(REST_URL + 1)
-//                .with(userHttpBasic(ADMIN)))
-//                .andDo(print())
-//                .andExpect(status().isUnprocessableEntity());
-//    }
+    @Test
+    void getById() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "/{id}", ADMIN_ID)
+                .with(userHttpBasic(ADMIN)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(contentJson(ADMIN));
+    }
+
+    @Test
+    void getNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + 1)
+                .with(userHttpBasic(ADMIN)))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void enable() throws Exception {
+        perform(MockMvcRequestBuilders.post(REST_URL + "/" + USER_ID)
+                .param("enabled", "false")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        assertFalse(userService.getById(USER_ID).isEnabled());
+    }
+
+    @Test
+    void createInvalid() throws Exception {
+        User expected = new User(null, null, "", "newPass", Role.USER, Role.ADMIN);
+        perform(MockMvcRequestBuilders.post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN))
+                .content(JsonUtil.writeValue(expected)))
+                .andExpect(status().isUnprocessableEntity())
+                .andDo(print());
+    }
 }

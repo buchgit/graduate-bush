395aef36712d6ecbced588dc027f2961c14accb0
buchgit <buchnorilsk@gmail.com>
2020-09-06 21:26:20 +0300

    delete /target .idea folders

--- src/test/java/ru/graduate/TestMatcher.java
+++ src/test/java/ru/graduate/TestMatcher.java
@@ -25,7 +25,7 @@ public class TestMatcher<T> {
                 (a, e) -> assertThat(a).isEqualToIgnoringGivenFields(e, fieldsToIgnore),
                 (a, e) -> assertThat(a).usingElementComparatorIgnoringFields(fieldsToIgnore).isEqualTo(e));
     }
-//test
+
     public void assertMatch(T actual, T expected) {
         assertion.accept(actual, expected);
     }

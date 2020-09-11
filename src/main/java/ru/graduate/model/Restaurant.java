6f0d2bbe87794a8e48016e3dc1ce23f7a9027456
buchgit <buchnorilsk@gmail.com>
2020-09-11 18:52:22 +0300

    +cache Restaurant

--- src/main/java/ru/graduate/model/Restaurant.java
+++ src/main/java/ru/graduate/model/Restaurant.java
@@ -1,6 +1,8 @@
 package ru.graduate.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Cache;
 
 import javax.persistence.*;
 import java.util.List;
@@ -15,6 +17,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "restaurants")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Restaurant extends AbstractNamedEntity {
 
     @OneToMany(mappedBy = "restaurant", fetch = FetchType.LAZY)
